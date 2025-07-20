912141e7135fec7647519b0bf95e2d545a7c2448
Kanan Kotwani <kanankotwani28@gmail.com>
2025-07-20 10:00:09 +0530

    Css chnages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,19 +1,21 @@
 import { HomePage } from './pages/HomePage'
-import { Routes , Route} from 'react-router'
+import { Routes , Route, BrowserRouter} from 'react-router-dom'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
 
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" 
         element={<HomePage />}>
       </Route>
-      <Route path="/"
-        element={<Checkout />}>
+      <Route path="checkout.html"
+        element={<CheckoutPage />}>
       </Route>
     </Routes>
-
-  )
+    </BrowserRouter>
+  );
 }
 export default App
