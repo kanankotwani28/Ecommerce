74223b4772ec15e6b48107cc7497fe0af1997b8f
Kanan Kotwani <kanankotwani28@gmail.com>
2025-07-23 17:03:36 +0530

    used data from theh backend to generate the HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { useEffect,useState } from 'react';
 import { Header } from "../assets/Header";
-import { products } from "../../starting-code/data/products.js";
+// import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
 export function HomePage() {
 
@@ -18,10 +19,15 @@ export function HomePage() {
   //   console.log(data);
   // });
 
-
-  axios.get('http://localhost:3000/api/products').then((response) =>{
-    console.log(response.data);
-  });
+  const [products,setProducts]=useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
+      .then((response) =>{
+        // console.log(response.data);
+        setProducts(response.data)
+      });
+  },[]);
+ 
   return (
     <>
       <title>Ecommerce Project</title>
