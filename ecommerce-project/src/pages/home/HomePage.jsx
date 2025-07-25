5cf6f44266d4f9c02280d348cdbd5ebea73a4146
Kanan Kotwani <kanankotwani28@gmail.com>
2025-07-25 18:50:19 +0530

    Async and Await added to the home page

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -21,16 +21,19 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    
+    const getHomeData = async() => {
+    const response = await axios.get("/api/products")
       // console.log(response.data);
       setProducts(response.data);
-    });
+    };
 
     //Generate the Cart Quantity
     // axios.get('http://localhost:3000/api/cart-items')
     //   .then((response)=>{
     //       setCart(response.data)
     //   });
+    getHomeData();
   }, []);
 
   return (
