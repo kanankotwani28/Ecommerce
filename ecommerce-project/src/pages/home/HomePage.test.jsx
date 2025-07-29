6686b1228dfed066758bfa88d5440ba5725ae92c
Kanan Kotwani <kanankotwani28@gmail.com>
2025-07-29 20:47:46 +0530

    Updated changes

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,7 +1,6 @@
 import {it,expect,describe,vi, beforeEach} from 'vitest';
 import {HomePage} from './HomePage'
 import {render,screen,within} from '@testing-library/react'
-import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { MemoryRouter } from 'react-router';
 
